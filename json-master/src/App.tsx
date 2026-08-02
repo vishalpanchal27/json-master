60a482a7f2a5a176e7dd442533eb825613f7a47b
vishalpanchal27 <112761001+vishalpanchal27@users.noreply.github.com>
2026-08-02 21:10:11 +0530

    Initial project setup

---
+++ json-master/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-950">
+      <h1 className="text-5xl font-bold text-cyan-400">
+        Developer Toolkit
+      </h1>
+    </div>
+  );
+}
+
+export default App;
